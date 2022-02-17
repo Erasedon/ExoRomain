b01ad30a081ba5788cbead4d8b7a9a247526030b
yanis.pirlet <yanis.pirlet@gmail.com>
2022-02-17 13:57:40 +0100

    premier version

---
+++ js/indexjs.js
@@ -0,0 +1,125 @@
+//exo 1 //
+var a = 5 ;
+console.log("Prendre une variables en php (la déclaration d'une variable se fait avec un $ en php), affecter une valeur à cette variable et l'afficher" , a);
+
+//exo1 //
+//exo 2//
+console.log("Prendre deux variables en php, affecter une valeur à chaque variable, afficher le résultat de l'addition, de la soustraction, et de la multiplication de ces deux variables");
+var nomb1 = 4; 
+var nomb2 = 5; 
+
+let resultat = nomb1 + nomb2; 
+
+console.log(resultat)
+
+let resultatmoins = nomb1 - nomb2; 
+
+console.log(resultatmoins)
+
+let resultatdiv = nomb1 / nomb2; 
+
+console.log(resultatdiv)
+
+let resultatmulti = nomb1 * nomb2; 
+
+console.log(resultatmulti)
+
+//exo 2 // 
+
+//exo 3 //
+
+console.log("Prendre une variable en php et affecter une chaine de caracètere à cette variable et l'afficher ");
+
+let chainecaract = "Coucou petite perruche,  !az!zaef az";
+
+console.log(chainecaract);
+
+// exo 3//
+// exo 4// 
+
+console.log("Afficher à l'aide des deux variables déjà défini le texte contenu dans les variables à l'aide d'une concaténation (un espace est souhaitée entre les deux variables) \n");
+let testa = "coucou";
+let testb = "petite perruche";
+
+console.log(testa ," ", testb);
+
+// exo 4//
+// exo 5 //
+
+
+console.log("Permutter le contenu de la variable a et de la variable b \n ");
+ var valeura = 20 ;
+ var valeurb = 30 ;
+
+console.log("La valeur de a est maintenat : ",valeura,", la valeur de b est maintenant : ",valeurb," \n");
+
+var c = valeura;
+var valeura = valeurb;
+var valeurb = c;
+
+console.log("La valeur de a est maintenat : ",valeura,", la valeur de b est maintenant : ",valeurb,"");
+
+// exo 6//
+
+console.log("A partir d'un algorithme, définir dans tout les cas possibles quels chiffre est le plus grand (vous pouvez changer les valeurs des variables a et b pour tester toutes les possiblités) \n");
+let vara = 40 ;
+let varb = 30 ;
+
+if (vara > varb){
+  console.log (" La variable ",vara," est plus grand que ",varb,"");
+} else if (varb > vara){
+  console.log (" La variable ",varb," est plus grand que ",vara,"");
+} else {
+  console.log(" La variables sont égaux : ",vara ,varb, "");
+}
+//exo 6// 
+//exo 7 //
+
+console.log ("faire une fonction qui prend en parametres deux variables ( a et b par exemple ) et qui définis quel chiffre est le plus grand \n");
+let toto = 30 ;
+let tata = 30 ;
+/* Code à ajouter ci-dessous */
+let result = comparaison( toto, tata);
+console.log (result);
+function comparaison(x,y){
+    
+    if (x > y){
+        resultat = " La variable "+x+" est plus grand que "+y+"";
+    } else if (y > x){
+      resultat =  " La variable "+y+" est plus grand que "+x+"";
+    } else {
+      resultat =" La variables sont égaux : "+x + y+" ";
+    }
+    return resultat;
+}
+// exo 7 //
+// exo 8 //
+console.log("afficher un nombre aléatoire à partir de la fonction math \n");
+const min  = 0
+const max = 100 
+
+var nombrealeatiore =  Math.floor(Math.random() * (max - min) + min);
+console.log(nombrealeatiore);
+// exo 8 //
+// exo 9// 
+console.log("afficher 10 nombre aléatoire à partir de la fonction math.random() en utilisant une boucle \n");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+console.log(getRandomInt(100));
+// exo 9// 
+// exo 10 //
+console.log("afficher des nombres aléatoires compris entre 1 et 100 et s'arreter quand le dernier chiffre est >90 \n");
+/* Code à ajouter ci-dessous */
+const min1 = 0
+const max1 = 100 
+
+var nombrealeatiore1 = 0 ;
+
+while( nombrealeatiore1 < 90){
+    nombrealeatiore1 = Math.floor(Math.random() * (max1 - min1) + min1);
+  console.log (nombrealeatiore1);
+  console.log("\n");
+}
+// exo 10 //
---
+++ my-app/app.py
@@ -0,0 +1,78 @@
+from cgi import print_environ
+
+# exo 1 #
+print("Prendre une variables en python, affecter une valeur à cette variable et l'afficher \n ") 
+a = 5 
+print(a ,"\n")
+# Exo 1 #
+# Exo 2 # 
+
+print("Prendre deux variables en php, affecter une valeur à chaque variable, afficher le résultat de l'addition, de la soustraction, et de la multiplication de ces deux variables \n") 
+a = 20
+b = 5  
+resultplus = a + b
+resultmoins = a - b
+resultdiv = a / b
+resultmulti = a * b
+
+print(resultplus)
+print(resultmoins)
+print(resultdiv)
+print(resultmulti, "\n")
+# Exo 2 #
+# Exo 3 # 
+
+print("Prendre une variable en php et affecter une chaine de caracètere à cette variable et l'afficher \n ") 
+ 
+
+chainecaract = "Coucou petite perruche,  !az!zaef az \n"
+
+print(chainecaract)
+# Exo 3 #
+# Exo 4 # 
+
+print("Afficher à l'aide des deux variables déjà défini le texte contenu dans les variables à l'aide d'une concaténation (un espace est souhaitée entre les deux variables \n") 
+
+testa = "coucou"
+testb = "petite perruche \n"
+
+print(testa ," ", testb)
+
+# Exo 4 #
+# Exo 5 # 
+
+print("Permutter le contenu de la variable a et de la variable b \n ") 
+
+valeura = 20 
+valeurb = 30 
+
+print("La valeur de a est maintenat : ",valeura,", la valeur de b est maintenant : ",valeurb," \n")
+
+c = valeura
+valeura = valeurb
+valeurb = c
+
+print("La valeur de a est maintenat : ",valeura,", la valeur de b est maintenant : ",valeurb,"\n")
+
+# Exo 5 #
+# Exo 6 # 
+
+print("Prendre deux variables en php, affecter une valeur à chaque variable, afficher le résultat de l'addition, de la soustraction, et de la multiplication de ces deux variables") 
+
+# Exo 6 #
+# Exo 7 # 
+
+print("Prendre deux variables en php, affecter une valeur à chaque variable, afficher le résultat de l'addition, de la soustraction, et de la multiplication de ces deux variables") 
+
+# Exo 8 #
+# Exo 9 # 
+
+print("Prendre deux variables en php, affecter une valeur à chaque variable, afficher le résultat de l'addition, de la soustraction, et de la multiplication de ces deux variables") 
+
+# Exo 9 #
+# Exo 10 # 
+
+print("Prendre deux variables en php, affecter une valeur à chaque variable, afficher le résultat de l'addition, de la soustraction, et de la multiplication de ces deux variables") 
+
+# Exo 10 #
+ 
